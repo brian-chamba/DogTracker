f578b8edc1754683fd75222d4768d125346a06e5
bchamba <brian.chamba@siriusxm.com>
2025-12-16 14:51:55 -0500

    index changed

---
+++ js/historyStorage.js
@@ -0,0 +1,23 @@
+﻿window.historyStorage = {
+    getHistory: function () {
+        try {
+            return window.localStorage.getItem('dogTrackerHistory');
+        } catch (e) {
+            return null;
+        }
+    },
+    saveHistory: function (json) {
+        try {
+            window.localStorage.setItem('dogTrackerHistory', json);
+        } catch (e) {
+            // ignore
+        }
+    },
+    clearHistory: function () {
+        try {
+            window.localStorage.removeItem('dogTrackerHistory');
+        } catch (e) {
+            // ignore
+        }
+    }
+};
